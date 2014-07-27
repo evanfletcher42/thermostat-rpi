401ef9888f71eb8eeb52d69afdb74bd6d8f6aaac
evanfletcher42 <evan.fletcher42@gmail.com>
2014-07-27 12:26:54 -0600

    Unicode string literals everywhere.

--- app/views.py
+++ app/views.py
@@ -1,16 +1,17 @@
+# -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	title = 'Thermostat v0.1'
-	indoorTemp 	= '73.0'
-	outdoorTemp = '68.7'
-	setpointTemp = '70.0'
-	opMode = 'OFF'
+	title = u'Thermostat v0.1'
+	indoorTemp 	= u'73.0'
+	outdoorTemp = u'68.7'
+	setpointTemp = u'70.0'
+	opMode = u'OFF'
 	explainStrings = [ #fake array of explain messages
-		"The set point is 70.0, because I'm not allowed to make it colder.",
-		"The operating mode is OFF because it's cooler than 70.0 outside and you should just open a window."
+		u"The set point is 70.0, because I'm not allowed to make it colder.",
+		u"The operating mode is OFF because it's cooler than 70.0 outside and you should just open a window."
 	]
-	return render_template("index.html", title=title, indoorTemp=indoorTemp, outdoorTemp=outdoorTemp, setpointTemp=setpointTemp, opMode = opMode, explainStrings=explainStrings )
+	return render_template(u"index.html", title=title, indoorTemp=indoorTemp, outdoorTemp=outdoorTemp, setpointTemp=setpointTemp, opMode = opMode, explainStrings=explainStrings )
