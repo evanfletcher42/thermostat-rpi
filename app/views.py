9dbe743631bfd83c6d78363b40ee4b04363dd46f
evanfletcher42 <evan.fletcher42@gmail.com>
2014-07-27 12:28:28 -0600

    Try adding degree symbols back in

--- app/views.py
+++ app/views.py
@@ -6,12 +6,12 @@ from app import app
 @app.route('/index')
 def index():
 	title = u'Thermostat v0.1'
-	indoorTemp 	= u'73.0'
-	outdoorTemp = u'68.7'
-	setpointTemp = u'70.0'
+	indoorTemp 	= u'73.0°'
+	outdoorTemp = u'68.7°'
+	setpointTemp = u'70.0°'
 	opMode = u'OFF'
 	explainStrings = [ #fake array of explain messages
-		u"The set point is 70.0, because I'm not allowed to make it colder.",
-		u"The operating mode is OFF because it's cooler than 70.0 outside and you should just open a window."
+		u"The set point is 70.0°, because I'm not allowed to make it colder.",
+		u"The operating mode is OFF because it's cooler than 70.0° outside and you should just open a window."
 	]
 	return render_template(u"index.html", title=title, indoorTemp=indoorTemp, outdoorTemp=outdoorTemp, setpointTemp=setpointTemp, opMode = opMode, explainStrings=explainStrings )
