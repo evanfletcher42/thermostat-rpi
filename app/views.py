49ae9dfcb490588fe342dd432462203ba3a8239d
evanfletcher42 <evan.fletcher42@gmail.com>
2014-07-27 11:31:09 -0600

    Remove yet another degree symbol.

--- app/views.py
+++ app/views.py
@@ -10,6 +10,6 @@ def index():
 	setpointTemp = '70.0'
 	explainStrings = [ #fake array of explain messages
 		"The set point is 70.0, because I'm not allowed to make it colder.",
-		"The operating mode is OFF because it's cooler than 70.0° outside and you should just open a window."
+		"The operating mode is OFF because it's cooler than 70.0 outside and you should just open a window."
 	]
 	return render_template("index.html", title=title, indoorTemp=indoorTemp, outdoorTemp=outdoorTemp, setpointTemp=setpointTemp, explainStrings=explainStrings )
