5ff97c32cd92b795ee2775dfb5dc8562917c0112
evanfletcher42 <evan.fletcher42@gmail.com>
2014-07-27 11:26:37 -0600

    Update views and templates to work nicely together.

--- app/views.py
+++ app/views.py
@@ -4,15 +4,12 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	user = { 'nickname': 'Evan'} #fake user
-	posts = [ #fake array of posts
-		{
-			'author': { 'nickname': 'John'}, 
-			'body': 'Beautiful day in Portland!'
-		}, 
-		{
-			'author': { 'nickname': 'Susan' },
-			'body': 'Test post please ignore'
-		}
+	title = 'Thermostat v0.1'
+	indoorTemp 	= '73.0°'
+	outdoorTemp = '68.7°'
+	setpointTemp = '70.0°'
+	explainStrings = [ #fake array of explain messages
+		"The set point is 70.0°, because I'm not allowed to make it colder.",
+		"The operating mode is OFF because it's cooler than 70.0° outside and you should just open a window."
 	]
-	return render_template("index.html", title = 'Home', user=user, posts = posts)
+	return render_template("index.html", title=title, indoorTemp=indoorTemp, outdoorTemp=outdoorTemp, setpointTemp=setpointTemp, explainStrings=explainStrings )
