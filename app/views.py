8fb78ba9a3c9ad9927a48e32a2be2f64e1d4d403
evanfletcher42 <evan.fletcher42@gmail.com>
2014-07-27 11:29:29 -0600

    Move degree symbol to template because Python can\'t handle it

--- app/views.py
+++ app/views.py
@@ -5,9 +5,9 @@ from app import app
 @app.route('/index')
 def index():
 	title = 'Thermostat v0.1'
-	indoorTemp 	= '73.0°'
-	outdoorTemp = '68.7°'
-	setpointTemp = '70.0°'
+	indoorTemp 	= '73.0'
+	outdoorTemp = '68.7'
+	setpointTemp = '70.0'
 	explainStrings = [ #fake array of explain messages
 		"The set point is 70.0°, because I'm not allowed to make it colder.",
 		"The operating mode is OFF because it's cooler than 70.0° outside and you should just open a window."
