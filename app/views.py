77008b577673affb9ec1a8d5feff6271822be5a1
evanfletcher42 <evan.fletcher42@gmail.com>
2014-07-27 11:36:24 -0600

    Fix templates / views.

--- app/views.py
+++ app/views.py
@@ -8,8 +8,9 @@ def index():
 	indoorTemp 	= '73.0'
 	outdoorTemp = '68.7'
 	setpointTemp = '70.0'
+	opMode = 'OFF'
 	explainStrings = [ #fake array of explain messages
 		"The set point is 70.0, because I'm not allowed to make it colder.",
 		"The operating mode is OFF because it's cooler than 70.0 outside and you should just open a window."
 	]
-	return render_template("index.html", title=title, indoorTemp=indoorTemp, outdoorTemp=outdoorTemp, setpointTemp=setpointTemp, explainStrings=explainStrings )
+	return render_template("index.html", title=title, indoorTemp=indoorTemp, outdoorTemp=outdoorTemp, setpointTemp=setpointTemp, opMode = opMode, explainStrings=explainStrings )
