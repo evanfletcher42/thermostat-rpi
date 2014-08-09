6893ba3f51ac365d811e68f8d931375b87e2e6d0
evanfletcher42 <evan.fletcher42@gmail.com>
2014-08-09 10:52:16 -0600

    import datetime

--- app/rtc/control.py
+++ app/rtc/control.py
@@ -1,14 +1,14 @@
 import pywapi
 import pprint
 import rfc822
+from datetime import datetime
 
 result = pywapi.get_weather_from_noaa('KBDU')
 
 location = result['location']
-dt 		 = datetime(rfc822.parsedate_tz(result['observation_time_rfc822']))
+dt		 =  datetime.fromtimestamp(rfc822.mktime_tz(rfc822.parsedate_tz(result['observation_time_rfc822'])))
 temp_c	 = float(result['temp_c'])
 
 print 'location: \t', location
-print 'datetime: \t', dt
-print 'RFC822:   \t', dt.rfc822()
+print 'datetime: \t', str(dt)
 print 'temp (c): ', temp_c
