1b77bb38be34a3e6dcc7af160cdce64a303ef656
evanfletcher42 <evan.fletcher42@gmail.com>
2014-08-09 10:47:51 -0600

    Add datetime parsing of NOAA data

--- app/rtc/control.py
+++ app/rtc/control.py
@@ -1,7 +1,14 @@
 import pywapi
 import pprint
+import rfc822
 
 result = pywapi.get_weather_from_noaa('KBDU')
 
-print 'location: ', result['location']
-print 'temp (c): ', float(result['temp_c'])
+location = result[location]
+dt 		 = rfc822.parsedate_tz(result['observation_time_rfc822'])
+temp_c	 = float(result['temp_c'])
+
+print 'location: \t', location
+print 'datetime: \t', dt
+print 'RFC822:   \t', dt.rfc822()
+print 'temp (c): ', temp_c
