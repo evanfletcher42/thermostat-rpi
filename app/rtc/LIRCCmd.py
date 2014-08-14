4331966e6806d873b15c7b5eb96205edc46a9212
evanfletcher42 <evan.fletcher42@gmail.com>
2014-08-13 19:56:21 -0600

    Add delay after sending LIRC commands - not all are making it through

--- app/rtc/LIRCCmd.py
+++ app/rtc/LIRCCmd.py
@@ -1,36 +1,49 @@
-import os
+import os, time
+
+__CMD_RECV_DELAY = 0.2
 
 def toggleOnOff():
     os.system("irsend SEND_ONCE GE_AirConditioner on_stop")
+    time.sleep(__CMD_RECV_DELAY)
     
 def setCoolMode():
     os.system("irsend SEND_ONCE GE_AirConditioner cool")
+    time.sleep(__CMD_RECV_DELAY)
     
 def setFanMode():
     os.system("irsend SEND_ONCE GE_AirConditioner fan")
+    time.sleep(__CMD_RECV_DELAY)
     
 def setFanHi():
     os.system("irsend SEND_ONCE GE_AirConditioner hi")
+    time.sleep(__CMD_RECV_DELAY)
     
 def setFanMed():
     os.system("irsend SEND_ONCE GE_AirConditioner mid")
+    time.sleep(__CMD_RECV_DELAY)
     
 def setFanLow():
     os.system("irsend SEND_ONCE GE_AirConditioner low")
+    time.sleep(__CMD_RECV_DELAY)
     
 def tempInc(n=1):
     for i in range(0,n):
         os.system("irsend SEND_ONCE GE_AirConditioner tempup")
+        time.sleep(__CMD_RECV_DELAY)
 
 def tempDec(n=1):
     for i in range(0,n):
         os.system("irsend SEND_ONCE GE_AirConditioner tempdown")
+        time.sleep(__CMD_RECV_DELAY)
     
 def timerOn():
     os.system("irsend SEND_ONCE GE_AirConditioner timeron")
+    time.sleep(__CMD_RECV_DELAY)
         
 def timerOff():
     os.system("irsend SEND_ONCE GE_AirConditioner timeroff")
+    time.sleep(__CMD_RECV_DELAY)
 
 def sleep():
     os.system("irsend SEND_ONCE GE_AirConditioner sleep")
+    time.sleep(__CMD_RECV_DELAY)
