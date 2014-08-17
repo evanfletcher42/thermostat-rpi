08e59df5824baad58805c2c7bc87fdcd2ae7a941
evanfletcher42 <evan.fletcher42@gmail.com>
2014-08-17 10:14:49 -0600

    tabs -> spaces

--- app/models.py
+++ app/models.py
@@ -2,11 +2,11 @@ from app import db
 
 # Contains a log of what the system has been doing.
 class OperationLog(db.Model):
-	id              = db.Column(db.Integer, primary_key = True)
-	time            = db.Column(db.DateTime, index=True, unique=True)
-	indoorTemp      = db.Column(db.Float)
+    id              = db.Column(db.Integer, primary_key = True)
+    time            = db.Column(db.DateTime, index=True, unique=True)
+    indoorTemp      = db.Column(db.Float)
     setpointTemp    = db.Column(db.Float)
-	state           = db.Column(db.Integer)
+    state           = db.Column(db.Integer)
 
 # Contains historical weather data.  One entry per unique observation.
 class WeatherData(db.Model):
@@ -17,10 +17,10 @@ class WeatherData(db.Model):
 # Contains the weather forecast for the next while (in case internet goes down)
 # TODO need to actually implement this
 class WeatherForecast(db.Model):
-	id          = db.Column(db.Integer, primary_key = True)
-	time        = db.Column(db.DateTime, index=True, unique = True)
-	temperature = db.Column(db.Float)
-	
+    id          = db.Column(db.Integer, primary_key = True)
+    time        = db.Column(db.DateTime, index=True, unique = True)
+    temperature = db.Column(db.Float)
+    
 # Valid commands for schedule.
 class Commands:
     DISABLE_ALL,    \       # 0  = Disable system entirely (everything off)
@@ -38,12 +38,12 @@ class Commands:
     
 # Contains information about scheduled events (i.e. disabling stuff while away)
 class Schedule(db.Model):
-	id          = db.Column(db.Integer, primary_key = True)
+    id          = db.Column(db.Integer, primary_key = True)
     
     # Start and end times for the first time this event took place.
     # For repeating events, new start/end times are based on these.
-	tStartFirst = db.Column(db.DateTime, index=True)
-	tEndFirst   = db.Column(db.DateTime, index=True)
+    tStartFirst = db.Column(db.DateTime, index=True)
+    tEndFirst   = db.Column(db.DateTime, index=True)
     
     # 0=never, 1=daily, 2=weekly, 3=monthly, 4=annual
     repeatType  = db.Column(db.Integer)
@@ -57,7 +57,7 @@ class Schedule(db.Model):
     tNextEnd    = db.Column(db.DateTime, index=True)
     
     # What this event is supposed to do.  Uses values from "commands" enum above.
-	command     = db.Column(db.Integer)
+    command     = db.Column(db.Integer)
     
     # Optional float argument for a command.  
     argument    = db.Column(db.Float)
