62dc72919ac702a1475432e79f236d5e9b7229ce
evanfletcher42 <evan.fletcher42@gmail.com>
2014-12-13 11:56:52 -0700

    Stop reading board temperature because we're doing nothing with it

--- thermometer.py
+++ thermometer.py
@@ -7,24 +7,29 @@ device_file = [device_folder[0] + '/w1_slave', device_folder[1] + '/w1_slave']
 
 def read_temp_raw():
     """Grabs raw temperature data from the sensors."""
-    f_1 = open(device_file[0], 'r')
-    lines_1 = f_1.readlines()
-    f_1.close()
+    
+    # Uncomment this to read board temperature
+    #f_1 = open(device_file[0], 'r')
+    #lines_1 = f_1.readlines()
+    #f_1.close()
+    
     f_2 = open(device_file[1], 'r')
     lines_2 = f_2.readlines()
     f_2.close()
-    return lines_1 + lines_2
+    
+    
+    return lines_2
 
 
 def read_temp():
     """Checks that the sensor connection was good and strips out the temperature.
     Returns a tuple of (board temperature, air temperature)."""
     lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES' or lines[2].strip()[-3:] != 'YES':
+    while lines[0].strip()[-3:] != 'YES':# or lines[2].strip()[-3:] != 'YES':
         print "error reading from sensor:"
         print lines
         time.sleep(0.2)
         lines = read_temp_raw()
-    equals_pos = lines[1].find('t='), lines[3].find('t=')
-    temp = float(lines[1][equals_pos[0]+2:])/1000, float(lines[3][equals_pos[1]+2:])/1000
+    equals_pos = lines[1].find('t=')
+    temp = None, float(lines[1][equals_pos+2:])/1000
     return temp
