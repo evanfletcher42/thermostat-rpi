2cd09285e6c094c06494546ef7678e71cf9dc0ed
evanfletcher42 <evan.fletcher42@gmail.com>
2015-05-31 13:33:04 -0600

    DB: Add support for new schedule type.

--- app/models.py
+++ app/models.py
@@ -20,44 +20,20 @@ class WeatherForecast(db.Model):
     id          = db.Column(db.Integer, primary_key = True)
     time        = db.Column(db.DateTime, index=True, unique = True)
     temperature = db.Column(db.Float)
-    
-# Valid commands for schedule.
-class Commands:
-    DISABLE_ALL,    \
-    DISABLE_HEAT,   \
-    DISABLE_COOL,   \
-    FORCE_SETPT,    \
-    FORCE_FAN_LOW,  \
-    FORCE_FAN_MED,  \
-    FORCE_FAN_HI,   \
-    FORCE_AC_LOW,   \
-    FORCE_AC_MED,   \
-    FORCE_AC_HI,    \
-    FORCE_HEAT = range(11)
-    # ... (add more here)
-    
-# Contains information about scheduled events (i.e. disabling stuff while away)
+        
+# Contains information about the weekly thermostat schedule.
+# "events" contain a start time and a low/high setpoint temperature.
+# The system queries this table for the closest time in the past and uses those settings.
+# Effectively, this means that when you pass an event, those settings are used until you pass another event.
 class Schedule(db.Model):
     id          = db.Column(db.Integer, primary_key = True)
     
-    # Start and end times for the first time this event took place.
-    # For repeating events, new start/end times are based on these.
-    tStartFirst = db.Column(db.DateTime, index=True)
-    tEndFirst   = db.Column(db.DateTime, index=True)
+    # Start time for the scheduled event.  
+    # Use actual day/time instead of "time since Sunday 12:00" to avoid DST awkwardness.
+    # Complicates controller query a little, but OH WELL.  
+    day         = db.Column(db.Integer, index=True)
+    time        = db.Column(db.Time, index=True)
     
-    # 0=never, 1=daily, 2=weekly, 3=monthly, 4=annual
-    repeatType  = db.Column(db.Integer)
-    
-    # End time of the last time this will repeat.  NONE if forever.
-    tEndLast    = db.Column(db.DateTime)
-    
-    # Start/end times for the next time this event will take place.
-    # Useful for fast db lookup / must be maintained by program
-    tNextStart  = db.Column(db.DateTime, index=True)
-    tNextEnd    = db.Column(db.DateTime, index=True)
-    
-    # What this event is supposed to do.  Uses values from "commands" enum above.
-    command     = db.Column(db.Integer)
-    
-    # Optional float argument for a command.  
-    argument    = db.Column(db.Float)
+    # Low/high setpoints.  
+    lowSetpoint = db.Column(db.Float)
+    highSetpoint = db.Column(db.Float)
